63b1de8c94d0767bb66ecb23e1445b68c4b9fe58
HuitziDomingo <false>
2023-10-13 23:44:40 -0600

    FIX rutas hijas en pages

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,14 +1,25 @@
 import { Routes, RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core'
 import { PagesComponent } from './pages.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { ProgressComponent } from './progress/progress.component'
+import { Grafica1Component } from './grafica1/grafica1.component'
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'dashborad',
     component: PagesComponent,
     children: [
-      {path: 'dashboard', component: },
-    ],
+      { path: '', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      
+    ]
   },
 ]
 
-export const PagesRoutes = RouterModule.forChild(routes)
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PageRoutingModule { }
